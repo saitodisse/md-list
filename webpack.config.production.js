1017662b4c265d72ea9fd3f6ad6cc498ff59958c
Julio Makdisse Saito <saitodisse@gmail.com>
2016-09-03 16:05:59 -0300

    [build] node_env

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -18,8 +18,7 @@ module.exports = {
     }),
     new webpack.DefinePlugin({
       'process.env': {
-        'ENV': JSON.stringify(process.env.ENV),
-        'NODE_ENV': JSON.stringify('production'),
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
         'API_KEY': JSON.stringify(process.env.API_KEY),
         'AUTH_DOMAIN': JSON.stringify(process.env.AUTH_DOMAIN),
         'DATABASE_URL': JSON.stringify(process.env.DATABASE_URL),
