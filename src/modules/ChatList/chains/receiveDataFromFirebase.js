fca4548361dc6be8ee962dba91b4f52858457b7b
Julio Makdisse Saito <saitodisse@gmail.com>
2016-09-08 13:05:24 -0300

    [notifications] debounce 200ms

--- src/modules/ChatList/chains/receiveDataFromFirebase.js
+++ src/modules/ChatList/chains/receiveDataFromFirebase.js
@@ -1,9 +1,23 @@
 import mergeItem from '../actions/mergeItem';
 import notificationItemAdd from '../actions/notificationItemAdd';
+import {debounce} from 'cerebral/operators';
 
 const receiveDataFromFirebase = [
   mergeItem,
-  notificationItemAdd,
+
+  // This action holds until
+  // A: 200ms has passed
+  //  - "accepted" is run
+  // B: the action is triggered again
+  //  - "discarded" is run on the previous execution
+  //  - the action on new execution is holding again
+  debounce(200), {
+    accepted: [
+      notificationItemAdd,
+    ],
+    discarded: []
+  },
+
 ];
 
 export default receiveDataFromFirebase;
