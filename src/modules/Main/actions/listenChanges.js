4ce096c4b0cd1de44a234790f79996f830d25da1
Julio Makdisse Saito <saitodisse@gmail.com>
2016-09-22 20:20:40 -0300

    [firebase] listenChanges refactored

--- src/modules/Main/actions/listenChanges.js
+++ src/modules/Main/actions/listenChanges.js
@@ -1,10 +1,8 @@
-const options = {
-  orderByChild: 'created_at',
-  startAt: (new Date()).getTime(),
-};
-
 function listenChanges({ services }) {
-  services.firebase.onChildAdded('items', 'chatList.firebaseChildAdded', options);
+  services.firebase.onChildAdded('items', 'chatList.firebaseChildAdded', {
+    orderByChild: 'created_at',
+    startAt: (new Date()).getTime(),
+  });
   services.firebase.onChildRemoved('items', 'chatList.firebaseChildRemoved', {});
   services.firebase.onChildChanged('items', 'chatList.firebaseChildChanged', {});
 }
