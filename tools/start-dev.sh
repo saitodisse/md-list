6de49df16e632593186877935be7389418fd6907
Julio Makdisse Saito <saitodisse@gmail.com>
2016-10-04 19:52:13 -0300

    [tasks] npm start comment task init

--- tools/start-dev.sh
+++ tools/start-dev.sh
@@ -3,19 +3,18 @@ set -e
 
 . .env-dev
 
+#NODE_ENV=$NODE_ENV                                     \
+#API_KEY=$API_KEY                                       \
+#AUTH_DOMAIN=$AUTH_DOMAIN                               \
+#DATABASE_URL=$DATABASE_URL                             \
+#STORAGE_BUCKET=$STORAGE_BUCKET                         \
+#ELASTIC_SEARCH_URI=$ELASTIC_SEARCH_URI                 \
+#FIREBASE_CRED_JSON_PATH=$FIREBASE_CRED_JSON_PATH       \
+#node ./queue/start.js & TASK_PID=$! &                  \
 NODE_ENV=$NODE_ENV                                     \
 API_KEY=$API_KEY                                       \
 AUTH_DOMAIN=$AUTH_DOMAIN                               \
 DATABASE_URL=$DATABASE_URL                             \
 STORAGE_BUCKET=$STORAGE_BUCKET                         \
 ELASTIC_SEARCH_URI=$ELASTIC_SEARCH_URI                 \
-FIREBASE_CRED_JSON_PATH=$FIREBASE_CRED_JSON_PATH       \
-./node_modules/.bin/webpack-dev-server & TASK_PID=$! & \
-NODE_ENV=$NODE_ENV                                     \
-API_KEY=$API_KEY                                       \
-AUTH_DOMAIN=$AUTH_DOMAIN                               \
-DATABASE_URL=$DATABASE_URL                             \
-STORAGE_BUCKET=$STORAGE_BUCKET                         \
-ELASTIC_SEARCH_URI=$ELASTIC_SEARCH_URI                 \
-FIREBASE_CRED_JSON_PATH=$FIREBASE_CRED_JSON_PATH       \
-node ./queue/start.js && kill $TASK_PID
+./node_modules/.bin/webpack-dev-server
