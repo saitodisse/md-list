9f476297cfadf9aadfd8695180829a1e10f141c3
Julio Makdisse Saito <saitodisse@gmail.com>
2016-10-05 17:52:21 -0300

    [crawler] jobs

--- src/components/Jobs/index.js
+++ src/components/Jobs/index.js
@@ -9,7 +9,36 @@ export default connect({}, {},
     render() {
       return (
         <div style={styles.container} className="container">
-          Jobs
+          <section className="title">
+            Jobs
+          </section>
+
+          <div style={styles.inputContainer} id="inputContainer">
+            <label style={styles.jobNameLabel}>
+              Job name:
+            </label>
+            <input
+              id="my_input"
+              type="text"
+              style={styles.jobNameInput}
+              ref={node => {
+                this.inputNode = node;
+              }}
+              value={this.props.input_search}
+              onChange={event => this.props.inputSearchChanged({ query: event.target.value })}
+              onKeyDown={this._OnKeyDown}
+            />
+            <button
+              style={styles.searchButton}
+              onClick={this._onSearch}
+            >
+              save
+            </button>
+          </div>
+
+          <div style={styles.resultsContainer} id="resultsContainer">
+            {/*<Items items={this.props.results} hideButtons={true}/>*/}
+          </div>
         </div>
       );
     }
--- src/components/Jobs/styles.js
+++ src/components/Jobs/styles.js
@@ -9,4 +9,31 @@ export default {
     marginRight: 10,
     overflowY: 'auto',
   },
+
+  inputContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    margin: '10px 25px',
+  },
+
+  jobNameLabel: {
+    fontSize: 19,
+    marginRight: 7,
+  },
+
+  jobNameInput: {
+    flexGrow: 1,
+  },
+
+  searchButton: {
+    flexGrow: 0,
+    width: '49px',
+    marginLeft: '20px',
+    cursor: 'pointer',
+    color: '#184c2e',
+    border: '1px solid #3ba93e',
+    backgroundColor: '#bdffbf',
+    fontSize: '16px',
+  },
 };
